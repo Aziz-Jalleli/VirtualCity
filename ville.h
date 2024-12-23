e869aa8c9f0514cfedf26b7821593af8ddb6384b
Aziz-Jalleli <azizjallali.99@gmail.com>
2024-12-23 15:30:56 +0100

    Your commit message

---
+++ ville.h
@@ -0,0 +1,24 @@
+#ifndef VILLE_H
+#define VILLE_H
+#include<string>
+#include <QObject>
+#include <vector>
+#include "batiment.h"
+using namespace std;
+class Ville: public QObject
+{
+    Q_OBJECT;
+private:
+    string nom;
+    float budget;
+    float population;
+    int satisfaction;
+    vector<Batiment> batiments;
+    string ressources;
+public:
+    Ville(QObject *parent = nullptr,string nom='', float budget =0,float population=0,int satisfaction =0,string ressources = "Eau");
+    void ajouterBatiment(Batiment b);
+    void supprimerBatiment(int id);
+};
+
+#endif // VILLE_H
